8901f21c12e66f3d485cb91fb41b58a2848895c7
Benjamin Simmers <bej1380@gmail.com>
2025-11-02 20:48:09 +1000

    fix: formatting

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/clean-react/",
-  plugins: [
-    react({
-      babel: {
-        plugins: ['babel-plugin-react-compiler'],
-      },
-    }),
-  ],
+    base: "/clean-react/",
+    plugins: [
+        react({
+            babel: {
+                plugins: ["babel-plugin-react-compiler"],
+            },
+        }),
+    ],
 });
